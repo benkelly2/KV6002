f6ce34f3de129a1de3b5d81c2d75e2dc20352ca3
EdDavies1 <79151803+EdDavies1@users.noreply.github.com>
2023-02-26 19:05:51 +0000

    commit

--- TrueNorth Surfing/src/shop.php
+++ TrueNorth Surfing/src/shop.php
@@ -1,26 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/shop.css">
-    <title>Shop Page</title>
-</head>
-<body>
-
-    <div class="header">
-            <a href="../index.php"><img class="logo-icon" src="../img/TrueNorthSurfClubLogo.png" alt="True North Surf Club Logo"/></a>
-            <div class="header-right">
-                <a href="../index.php">Home</a>
-                <a href="gallery.php">Gallery</a>
-                <a href="contact.php">Contact Us</a>
-                <a href="signup.php">Sign Up</a>
-                <a href="members.php">For Members</a>
-                <a class="active" href="shop.php">Shop</a>
-            </div>
-        </div>
-
-        <h1>Shop</h1>
-    
-</body>
-</html>
+<?php
+    ini_set("session.save_path", "WHEREVER THE SESSION DATA FILE WILL BE");
+    session_start();
+    include("scripts/functions.php");
+    echo makePageStart("True North Surf Club Shop", "shop.css")
+    echo makeMain("True North Surf Club Shop");
+    echo makeNav("Navigation Menu");
+    echo makeNavOptions(array("index.php" => "Home", "gallery.php" => "Gallery", "shop.php" => "TNSC Shop", "members.php" => "Members Area", "signup.php" => "Sign Up"));
+    echo makeMain("True North Surf Club Shop");
+    echo isLogged();
+    echo makeBody("Shop Items etc");
+    echo endBody();
+    echo makeFooter("True North Surf Club Merchandise Shop 2023")
+    ?>
