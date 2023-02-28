70a63fa4d8a80570922b5ed3847f03eb83800244
EdDavies1 <79151803+EdDavies1@users.noreply.github.com>
2023-02-28 14:42:50 +0000

    Update shop.php

--- TrueNorth Surfing/src/shop.php
+++ TrueNorth Surfing/src/shop.php
@@ -4,6 +4,6 @@
     include("scripts/updated_functions.php");
     echo headSetup("../css/shop.css");
     echo bodyStartSetup();
-    echo genNav(array("index.php" => "Home", "gallery.php" => "Gallery", "contact.php" => "Contact Us", "signup.php" => "Sign Up", "members.php" => "For Members", "newshop.php" => "Shop"));
+    echo genNav(array("index.php" => "Home", "gallery.php" => "Gallery", "contact.php" => "Contact Us", "signup.php" => "Sign Up", "members.php" => "For Members", "shop.php" => "Shop"));
     echo bodyClose("True North Surf Club Shop:");
     ?>
