23e00f9fb4750e5bb1a3f5cc5c46da263f79cce5
EdDavies1 <79151803+EdDavies1@users.noreply.github.com>
2023-02-28 14:38:14 +0000

    commit

--- TrueNorth Surfing/src/shop.php
+++ TrueNorth Surfing/src/shop.php
@@ -1,13 +1,9 @@
 <?php
     //ini_set("session.save_path", "WHEREVER THE SESSION DATA FILE WILL BE");
     session_start();
-    include("scripts/functions.php");
-    echo makePageStart("True North Surf Club Shop", "../css/shop.css");
-    echo makeNav("Navigation Menu");
-    echo makeNavOptions(array("index.php" => "Home", "gallery.php" => "Gallery", "shop.php" => "TNSC Shop", "members.php" => "Members Area", "signup.php" => "Sign Up"));
-    echo makeMain('<a href="../index.php"><img class="logo-icon" src="../img/TrueNorthSurfClubLogo.png" alt="True North Surf Club Logo"/></a>');
-    echo isLogged();
-    echo makeBody("Shop Items etc");
-    echo endBody();
-    echo makeFooter("True North Surf Club Merchandise Shop 2023")
+    include("scripts/updated_functions.php");
+    echo headSetup("../css/shop.css");
+    echo bodyStartSetup();
+    echo genNav(array("index.php" => "Home", "gallery.php" => "Gallery", "contact.php" => "Contact Us", "signup.php" => "Sign Up", "members.php" => "For Members", "newshop.php" => "Shop"));
+    echo bodyClose("True North Surf Club Shop:");
     ?>
