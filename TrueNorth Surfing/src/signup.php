fb019a0edf8d75539b8eac219ddb521c78254092
benkelly2 <97699043+benkelly2@users.noreply.github.com>
2023-03-02 15:53:37 +0000

    Testing google form in signup page

--- TrueNorth Surfing/src/signup.php
+++ TrueNorth Surfing/src/signup.php
@@ -26,8 +26,22 @@
 
         <h2>Sign-Up Form</h2>
         <p>Sign up to become a True North member here...</p>
+
+        <!-- This will need to be swapped with the TNSC google signup form -->
+        <iframe 
+            src="https://docs.google.com/forms/d/e/1FAIpQLSc1ffmwuMHNLCSQq6YtElYKni3DgyINzYbPN_EBDzLlTR1SHw/viewform?embedded=true" 
+            width="640" 
+            height="1280" 
+            frameborder="0" 
+            marginheight="0" 
+            marginwidth="0"
+            
+        >
+            Loading…
+        </iframe>
         
-        <div class="form-container" id="sign-up-form"> <!-- Change action to php script when ready -->    
+        <!-- START OF FORM - PROBS WONT USE -------------------------->
+        <!-- <div class="form-container" id="sign-up-form">  
             <form action="./scripts/processForm.php">
                 <div>
                     <label class="firstnameLabel">First Name</label>
@@ -56,7 +70,9 @@
 
                 <input type="submit" class="submit" value="Submit">
             </form>
-        </div>
+        </div> -->
+        <!-- END OF FORM - PROBS WONT USE -------------------------->
+
     </div>
 
 </body>
