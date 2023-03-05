3d10f0e1abb5dc396dbcb4ff3fc9325c7f816e92
wyciongzfiuta <filipowicz.k@gmail.com>
2023-03-05 17:24:16 +0000

    Merge branch 'main' of https://github.com/benkelly2/KV6002 into main

--- TrueNorth Surfing/src/signup.php
+++ TrueNorth Surfing/src/signup.php
@@ -21,48 +21,12 @@
     </div>
 
     <!-- <h1>Sign-Up Page</h1> -->     
-<<<<<<< HEAD
         <div class="box">
         <div>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdI7BTC_c9MHynQfB8SoPyGgMY0m-b5uuZwmUwge26qqdBOlg/viewform" width="60%" height="1200" style="border:1px solid black;">
             </iframe>
         </div>
         </div>
-=======
-        <!-- START OF FORM - PROBS WONT USE -------------------------->
-        <!-- <div class="form-container" id="sign-up-form">  
-            <form action="./scripts/processForm.php">
-                <div>
-                    <label class="firstnameLabel">First Name</label>
-                    <input type="text" class="firstname" placeholder="Enter First Name" name="firstname" required>
-                </div>
-
-                <div>
-                    <label class="lastnameLabel">Surname</label>
-                    <input type="text" class="lastname" placeholder="Enter Surname" name="surname" required>
-                </div>
-
-                <div>
-                    <label class="emailLabel">Email</label>
-                    <input type="text" class="email" placeholder="Enter Email" name="email" required>
-                </div>
-
-                <div>
-                    <label class="passwordLabel">Password</label>
-                    <input type="password" class="password" placeholder="Enter Password" name="password " required>
-                </div>
-
-                <div>
-                    <label class="passwordLabel2">Re-enter password</label>
-                    <input type="password" class="password2" placeholder="Re-enter Password" name="password2" required>
-                </div>
-
-                <input type="submit" class="submit" value="Submit">
-            </form>
-        </div> -->
-        <!-- END OF FORM - PROBS WONT USE -------------------------->
-
->>>>>>> 0eec730214a37278b5ea2067dc9fb727b11ca870
     </div>
 
 </body>
