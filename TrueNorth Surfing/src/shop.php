d713c5361093a9682f909e12677fdb596773e166
EdDavies1 <79151803+EdDavies1@users.noreply.github.com>
2023-03-07 10:27:14 +0000

    image inclusion
    image inclusion

--- TrueNorth Surfing/src/shop.php
+++ TrueNorth Surfing/src/shop.php
@@ -7,5 +7,8 @@
     echo genNav(array("index.php" => "Home", "gallery.php" => "Gallery", "contact.php" => "Contact Us", "signup.php" => "Sign Up", "members.php" => "For Members", "shop.php" => "Shop"));
     echo headerClose();
     echo bodyStart("True North Surf Club Shop:");
+    ?>
+
+    <?php
     echo bodyEnd();
     ?>
