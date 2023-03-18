10d6b80beef7820d425f837276d76b903a6f486c
EdDavies1 <79151803+EdDavies1@users.noreply.github.com>
2023-03-18 15:43:53 +0000

    commit

--- TrueNorth Surfing/src/shop.php
+++ TrueNorth Surfing/src/shop.php
@@ -7,8 +7,16 @@
     echo genNav(array("index.php" => "Home", "gallery.php" => "Gallery", "contact.php" => "Contact Us", "signup.php" => "Sign Up", "members.php" => "For Members", "shop.php" => "Shop"));
     echo headerClose();
     echo bodyStart("True North Surf Club Shop:");
-    ?>
-
-    <?php
+?>
+<div class="Shop-Item">
+    <div id="shop-img">
+        <h3>TNSC tshirt</h3>
+        <img src="../TNSC_Pictures/TNSC_tshirt/11fdc54b-48c6-42f6-ac37-9dc6fe538ef6.jpeg">
+    </div>
+    <div class="shop-desc">
+        Description: TNSC T-Shirt is a vibe!!!!
+    </div>
+</div>
+<?php
     echo bodyEnd();
-    ?>
+?>
