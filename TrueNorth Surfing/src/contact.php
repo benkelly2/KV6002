3d585be3070053f4039108b0912d3bbea586a7c5
wyciongzfiuta <filipowicz.k@gmail.com>
2023-03-21 15:08:52 +0000

    contact form

--- TrueNorth Surfing/src/contact.php
+++ TrueNorth Surfing/src/contact.php
@@ -1,62 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/contact.css">
-    <script type="text/javascript" src="./scripts/contact_form.js"></script>
-    <title>Contact</title>
-</head>
-<body>
-
-<<<<<<< HEAD
-    <div class="header">
-        <a href="../index.php"><img class="logo-icon" src="../img/TrueNorthSurfClubLogo.png" alt="True North Surf Club Logo"/></a>
-        <div class="header-right">
-            <a href="../index.php">Home</a>
-            <a href="gallery.php">Gallery</a>
-            <a class="active" href="contact.php">Contact Us</a>
-            <a href="signup.php">Sign Up</a>
-            <a href="members.php">For Members</a>
-            <a href="shop.php">Shop</a>
-        </div>
-    </div>
-
-    <h1>Contact Page</h1>
-
-    <div class="form-container" id="contactform">  
-        <form action="./scripts/processForm.php" method="post" name="contact_form">
-=======
 <?php
-    //ini_set("session.save_path", "WHEREVER THE SESSION DATA FILE WILL BE");
-    session_start();
-    include("scripts/functions.php");
-    echo headSetup("../css/contact.css");
-    echo headerSetup();
-    echo genNav(array("index.php" => "Home", "gallery.php" => "Gallery", "contact.php" => "Contact Us", "signup.php" => "Sign Up", "members.php" => "For Members", "shop.php" => "Shop"));
-    echo headerClose();
-    echo bodyStart("Contact Form:");
+//ini_set("session.save_path", "WHEREVER THE SESSION DATA FILE WILL BE");
+session_start();
+include("scripts/functions.php");
+echo headSetup("../css/contact.css");
+echo headerSetup();
+echo genNav(array("index.php" => "Home", "gallery.php" => "Gallery", "contact.php" => "Contact Us", "signup.php" => "Sign Up", "members.php" => "For Members", "shop.php" => "Shop"));
+echo headerClose();
+echo bodyStart("Contact Us");
+if (!isset($_GET['message'])) {
 ?>
-    <div class="form-container" id="sign-up-form">  
-        <form action="./scripts/processForm.php">
->>>>>>> cca98f154c0cc7d9d6b3203350ad6c4e8ea25473
-            <div>
-                <label class="nameLabel">Your Name</label>
-                <input type="text" class="name" placeholder="Enter Your Name" name="name" required>
-            </div>
+<div class="form-container" id="contactform">  
+    <form action="./processForm.php" method="post" name="contact_form">
+        <div>
+            <label class="nameLabel">Your Name</label>
+            <input type="text" placeholder="Enter Your Name" name="name" required>
+        </div>
 
-            <div>
-                <label class="emailLabel">Email</label>
-                <input type="text" class="email" placeholder="Enter Email" name="email" required>
-            </div>
+        <div>
+            <label class="emailLabel">Email</label>
+            <input type="text" placeholder="Enter Email" name="email" required>
+        </div>
 
-            <div>
-                <label class="messageLabel">Message</label>
-                <input type="text" class="message" placeholder="Enter Message" name="message" required>
-            </div>
-            <input type="submit" class="submit" value="Submit">
-        </form>
-    </div>
+        <div>
+            <label class="messageLabel">Message</label>
+            <input type="text" placeholder="Enter Message" name="message" required>
+        </div>
+        <input type="submit" class="submit" value="Submit">
+    </form>
+</div>
 <?php
+}
+if (isset($_GET['message'])) {
+    // get the message and sanitize it
+    $message = htmlspecialchars($_GET['message']);
+    
+    // display the message in a pop-up
+    if($_GET['message'] == 'Email sent successfully'){
+        echo '<script>alert("Email sent successfully.")</script>';
+    }
+    else{
+        echo '<script>alert("Error sending email. You will be redirected in 10 seconds.");setTimeout(function(){location.href="contact.php";},10000);</script>';
+    }
+?>
+<div>thanks</div>
+<?php
+}
 echo bodyEnd();
 ?>
--- TrueNorth Surfing/src/scripts/processForm.php
+++ TrueNorth Surfing/src/scripts/processForm.php
@@ -1,37 +1,56 @@
 <?php
 
-$errors = '';
-$myemail = 'yourname@website.com';
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/scripts/PHPMailer-master/src/PHPMailer.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/scripts/PHPMailer-master/src/SMTP.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/scripts/PHPMailer-master/src/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+	$mail = new PHPMailer(true);
+
+	$send_using_gmail = true;
+	//Send mail using gmail
+	if($send_using_gmail){
+		$mail->IsSMTP(); // telling the class to use SMTP
+		$mail->SMTPAuth = true; // enable SMTP authentication
+		$mail->SMTPDebug = 2;
+		$mail->SMTPSecure = 'tls';
+		$mail->Host = 'smtp.gmail.com';
+		$mail->Port = '587';
+		$mail->Sender = $email;
+		$mail->Username = "kv5732221@gmail.com"; // GMAIL username
+		$mail->Password = "szccudtechbzdmqw"; // app password
+	}
+	
+	//Typical mail data
+	$mail->AddAddress('kv5732221@gmail.com','admin');
+	$mail->AddReplyTo($email, $name);
+	$mail->Subject = "Message from $name";
+	$mail->Body = "$message";
+	
+	
+	try {
+		// send the email
+		$mail->Send();		
+		// redirect to contact page with success message
+		header("Location: contact.php?message=Email+sent+successfully");
+		exit();
+		
+	} catch (Exception $e) {
+		// something went wrong
+		$error = "Error sending email. " . $mail->ErrorInfo;
+		// redirect to contact page with error message
+		header("Location: contact.php?message=" . urlencode($error));
+		exit();
+	}
+	
 }
 
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-if( empty($errors))
-{
-	$to = $myemail; 
-	$email_subject = "Contact form submission: $name";
-	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
-	
-	$headers = "From: $myemail\n"; 
-	$headers .= "Reply-To: $email_address";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	//redirect to the 'thank you' page
-	header('Location: contact-form-thank-you.html');
-} 
 ?>
