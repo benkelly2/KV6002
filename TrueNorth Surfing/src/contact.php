56f0d39f4aa78d0af93d73791faa3db0e1d49441
wyciongzfiuta <filipowicz.k@gmail.com>
2023-03-21 15:20:31 +0000

    contact

--- TrueNorth Surfing/src/contact.php
+++ TrueNorth Surfing/src/contact.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 //ini_set("session.save_path", "WHEREVER THE SESSION DATA FILE WILL BE");
 session_start();
@@ -43,38 +42,8 @@ if (isset($_GET['message'])) {
         echo '<script>alert("Error sending email. You will be redirected in 10 seconds.");setTimeout(function(){location.href="contact.php";},10000);</script>';
     }
 ?>
-<div>thanks</div>
-=======
-<?php
-    //ini_set("session.save_path", "WHEREVER THE SESSION DATA FILE WILL BE");
-    session_start();
-    include("scripts/functions.php");
-    echo headSetup("TNSC - Contact", "../css/contact.css");
-    echo headerSetup();
-    echo genNav(array("index.php" => "Home", "gallery.php" => "Gallery", "contact.php" => "Contact Us", "signup.php" => "Sign Up", "members.php" => "For Members", "shop.php" => "Shop", "admin.php" => "Admin"));
-    echo headerClose();
-    echo bodyStart("Contact Form:");
-?>
-    <div class="form-container" id="contact-form">  
-        <form action="./scripts/processForm.php">
-            <div>
-                <label class="nameLabel">Your Name</label>
-                <input type="text" class="name" placeholder="Enter Your Name" name="name" required>
-            </div>
+<div>thanksss</div>
 
-            <div>
-                <label class="emailLabel">Email</label>
-                <input type="text" class="email" placeholder="Enter Email" name="email" required>
-            </div>
-
-            <div>
-                <label class="messageLabel">Message</label>
-                <input type="text" class="message" placeholder="Enter Message" name="message" required>
-            </div>
-            <input type="submit" class="submit" value="Submit">
-        </form>
-    </div>
->>>>>>> 0b687a5a2d5f2f8d063788c25026581b82f0bb79
 <?php
 }
 echo bodyEnd();
