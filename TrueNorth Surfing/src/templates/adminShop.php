30b218939d7adc54e9df1ad12085725d9e78e30c
charliecallister <callister.charlie@googlemail.com>
2023-03-29 16:01:05 +0100

    adding shop admin functions

--- TrueNorth Surfing/src/templates/adminShop.php
+++ TrueNorth Surfing/src/templates/adminShop.php
@@ -5,7 +5,7 @@
     <h1><?php echo $results['pageTitle']?></h1>
 
 <form action="admin.php?action=<?php echo $results['formAction']?>" method="post">
-    <input type="hidden" name="code_id" value="<?php echo $results['discount']->code_id ?>"/>
+    <input type="hidden" name="product_id" value="<?php echo $results['product']->product_id ?>"/>
 
 <?php if ( isset( $results['errorMessage'] ) ) { ?>
         <div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
@@ -14,20 +14,20 @@
     <ul>
 
     <li>
-    <label for="code_company">Company:</label>
-    <input type="text" id="code_company" name="code_company" value="<?php echo $results['discount']->code_company; ?>">
+    <label for="title">Company:</label>
+    <input type="text" id="title" name="title" value="<?php echo $results['product']->title; ?>">
     </li>
 
     <li>
-    <label for="code_content">Code:</label>
-    <input type="text" id="code_content" name="code_content" value="<?php echo $results['discount']->code_content; ?>">
+    <label for="description">Code:</label>
+    <input type="text" id="description" name="description" value="<?php echo $results['product']->description; ?>">
     </li>
 
     <li>
     <label for="code_display">Display Code:</label>
     <select id="form_code_display" name="code_display">
-    <option value="1" <?php echo ($results['discount']->code_display == 1) ? 'selected' : ''; ?>>Yes</option>
-    <option value="0" <?php echo ($results['discount']->code_display == 0) ? 'selected' : ''; ?>>No</option>
+    <option value="1" <?php echo ($results['product']->code_display == 1) ? 'selected' : ''; ?>>Yes</option>
+    <option value="0" <?php echo ($results['product']->code_display == 0) ? 'selected' : ''; ?>>No</option>
     </select>
     </li>
   
@@ -39,7 +39,7 @@
     </div>
     </form>
 
-<?php if ( $results['discount']->code_id ) { ?>
-      <p><a href="admin.php?action=deleteCode&amp;code_id=<?php echo $results['discount']->code_id ?>" onclick="return confirm('Delete This Code?')">Delete This Code</a></p>
+<?php if ( $results['product']->code_id ) { ?>
+      <p><a href="admin.php?action=deleteCode&amp;code_id=<?php echo $results['product']->code_id ?>" onclick="return confirm('Delete This Code?')">Delete This Code</a></p>
 <?php } ?>
 
