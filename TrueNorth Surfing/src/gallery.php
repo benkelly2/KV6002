6c005420f6747633ca2673cae9dbd81bf4425935
wyciongzfiuta <filipowicz.k@gmail.com>
2023-04-08 20:58:26 +0100

    uncommented chatbot oops

--- TrueNorth Surfing/src/gallery.php
+++ TrueNorth Surfing/src/gallery.php
@@ -93,4 +93,4 @@ echo genFooter(array("cookies.php" => "Cookies Policy", "privacy.php" => "Privac
 echo bodyEnd();
 ?>
 
-<?php //include 'chatbot/chatbot.html'; ?>
+<?php include 'chatbot/chatbot.html'; ?>
